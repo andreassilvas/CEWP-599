a066ee40ec19fb182c652d3eb4bb200ada3f2448
unknown <a_silvas@concordia.ca>
2017-10-19 22:01:12 -0400

    Codes from tonight 20171019

--- source/api/loader.php
+++ source/api/loader.php
@@ -1,24 +1,27 @@
 <?php
 
-require_once __DIR__.'/models/base_model.php';
+//require_once __DIR__.'/models/base_model.php';
 
 // Item
-require_once __DIR__.'/models/item_model.php';
-require_once __DIR__.'/views/item_view.php';
-require_once __DIR__.'/controllers/item_controller.php';
+//require_once __DIR__.'/models/item_model.php';
+//require_once __DIR__.'/views/item_view.php';
+//require_once __DIR__.'/controllers/item_controller.php';
 
 // Category
-require_once __DIR__.'/models/category_model.php';
-require_once __DIR__.'/controllers/category_controller.php';
+//require_once __DIR__.'/models/category_model.php';
+//require_once __DIR__.'/controllers/category_controller.php';
 
 
-// spl_autoload_register(function ($class_name) {
+spl_autoload_register(function ($class_name) {
+   
+    // ItemMOdel     
+   // model/item_model.php
 
-//     preg_match_all('/[A-Z][a-z]+/', $class_name, $matches);
+     preg_match_all('/[A-Z][a-z]+/', $class_name, $matches);
 
-//     $paths = $matches[0];
-//     $paths = array_values(array_filter($paths));
-//     $paths = array_map('strtolower', $paths);
+     $paths = $matches[0];
+     $paths = array_values(array_filter($paths));
+     $paths = array_map('strtolower', $paths);
 
-//     include __dir__."/{$paths[1]}s/{$paths[0]}_{$paths[1]}.php";
-// });
+     include __dir__."/{$paths[1]}s/{$paths[0]}_{$paths[1]}.php";
+ });
